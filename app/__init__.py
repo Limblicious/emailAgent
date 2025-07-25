2f5862047bf363ce5a1563323d91cfa1609a7446
Limblicious <limblicious@gmail.com>
2025-07-25 15:44:07 -0700

    Add .env and gitignore, added chatgpt key functionality to top of flask app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # loads variables from .env into os.environ
+
+# Now access it like:
+api_key = os.getenv("OPENAI_API_KEY")
+
 
 app = Flask(__name__)
 CORS(app)
