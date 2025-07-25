b35e718a6b396c6e50b285b25914a1a7652cb097
Limbs <lee.j.jonathan@gmail.com>
2025-07-25 13:41:53 -0700

    Initialize Flask project

---
+++ app/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+from . import routes  # noqa: E402,F401
+
+__all__ = ["app"]
---
+++ app/routes.py
@@ -0,0 +1,8 @@
+from flask import jsonify
+
+from . import app
+
+
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
